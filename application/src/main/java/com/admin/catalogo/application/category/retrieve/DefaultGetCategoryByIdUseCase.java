15bcc4e206f523a3ef462cd989b57ba356587352
Sonny Marinho <sonny.marinho@gmail.com>
2023-01-04 20:19:34 -0300

    :sparkles: feat: get category

--- application/src/main/java/com/admin/catalogo/application/category/retrieve/DefaultGetCategoryByIdUseCase.java
+++ application/src/main/java/com/admin/catalogo/application/category/retrieve/DefaultGetCategoryByIdUseCase.java
@@ -1,4 +1,33 @@
 package com.admin.catalogo.application.category.retrieve;
 
-public class DefaultGetCategoryByIdUseCase {
+import com.admin.catalogo.application.UseCase;
+import com.admin.catalogo.application.category.retrieve.get.CategoryOutput;
+import com.admin.catalogo.domain.category.CategoryGateway;
+import com.admin.catalogo.domain.category.CategoryID;
+import com.admin.catalogo.domain.exceptions.DomainException;
+import com.admin.catalogo.domain.validation.Error;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class DefaultGetCategoryByIdUseCase extends UseCase<String, CategoryOutput> {
+
+    private final CategoryGateway categoryGateway;
+
+    public DefaultGetCategoryByIdUseCase(CategoryGateway categoryGateway) {
+        this.categoryGateway = Objects.requireNonNull(categoryGateway);
+    }
+
+    @Override
+    public CategoryOutput execute(String anIn) {
+        CategoryID categoryID = CategoryID.from(anIn);
+
+        return categoryGateway.findById(categoryID)
+                .map(CategoryOutput::from)
+                .orElseThrow(notFound(categoryID));
+    }
+
+    private static Supplier<DomainException> notFound(CategoryID anId) {
+        return () -> DomainException.with(new Error("Category with ID %s was not found".formatted(anId.getValue())));
+    }
 }
--- application/src/test/java/com/admin/catalogo/application/category/retrieve/GetCategoryByIdUseCaseTest.java
+++ application/src/test/java/com/admin/catalogo/application/category/retrieve/GetCategoryByIdUseCaseTest.java
@@ -1,18 +1,27 @@
 package com.admin.catalogo.application.category.retrieve;
 
-import com.admin.catalogo.application.category.delete.DefaultDeleteCategoryUseCase;
+import com.admin.catalogo.application.category.retrieve.get.CategoryOutput;
+import com.admin.catalogo.domain.category.Category;
 import com.admin.catalogo.domain.category.CategoryGateway;
+import com.admin.catalogo.domain.category.CategoryID;
+import com.admin.catalogo.domain.exceptions.DomainException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 class GetCategoryByIdUseCaseTest {
 
-    private DefaultDeleteCategoryUseCase useCase;
+
+    @InjectMocks
+    private DefaultGetCategoryByIdUseCase useCase;
 
     @Mock
     CategoryGateway categoryGateway;
@@ -23,4 +32,61 @@ class GetCategoryByIdUseCaseTest {
         Mockito.reset(categoryGateway);
     }
 
+    @Test
+    void givenAValidId_whenCallsDeleteCategory_shouldBeOK() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedState = true;
+
+        Category aCategory = Category.newCategory(expectedName, expectedDescription);
+        var expectedId = aCategory.getId();
+
+        Mockito.when(categoryGateway.findById(Mockito.eq(expectedId))).thenReturn(Optional.of(aCategory.clone()));
+
+        final var actualCategory = useCase.execute(expectedId.getValue());
+
+        Assertions.assertEquals(actualCategory.id(), expectedId);
+        Assertions.assertEquals(actualCategory.name(), expectedName);
+        Assertions.assertEquals(actualCategory.description(), expectedDescription);
+        Assertions.assertEquals(actualCategory.isActive(), expectedState);
+        Assertions.assertEquals(actualCategory.createdAt(), aCategory.getCreatedAt());
+        Assertions.assertEquals(actualCategory.updatedAt(), aCategory.getUpdatedAt());
+        Assertions.assertEquals(actualCategory.deletedAt(), aCategory.getDeletedAt());
+        Assertions.assertEquals(CategoryOutput.from(aCategory), actualCategory);
+    }
+
+    @Test
+    void givenAValidId_whenCallsDeleteCategory_shouldReturnNotFound() {
+        final var expectedId = CategoryID.from("123");
+
+        final var expectedErrorMessage = "Category with ID %s was not found".formatted(expectedId.getValue());
+
+        Mockito.when(categoryGateway.findById(Mockito.eq(expectedId))).thenReturn(Optional.empty());
+
+        final var actualException = Assertions.assertThrows(
+                DomainException.class,
+                () -> useCase.execute(expectedId.getValue())
+        );
+
+        final var actualErrorMessage = actualException.getMessage();
+
+        Assertions.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
+
+    @Test
+    void givenAValidId_whenTheGatewayThrowsException_shouldReturnException() {
+        final var expectedId = CategoryID.from("123");
+        final var expectedErrorMessage = "Supposed message";
+
+        Mockito.when(categoryGateway.findById(Mockito.eq(expectedId)))
+                .thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        final var actualException = Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> useCase.execute(expectedId.getValue())
+        );
+
+        final var actualErrorMessage = actualException.getMessage();
+        Assertions.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
 }
