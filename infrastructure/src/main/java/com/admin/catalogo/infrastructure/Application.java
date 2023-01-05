241b451f64dd5855c92e19cbefabbc88624f1495
Sonny Marinho <sonny.marinho@gmail.com>
2023-01-04 23:15:24 -0300

    :gear: chore: h2 configuration for tests

--- infrastructure/src/main/java/com/admin/catalogo/infrastructure/Application.java
+++ infrastructure/src/main/java/com/admin/catalogo/infrastructure/Application.java
@@ -5,12 +5,13 @@ import com.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
+import static org.springframework.core.env.AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        System.setProperty(DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
--- infrastructure/src/test/java/com/admin/catalogo/infrastructure/ApplicationTest.java
+++ infrastructure/src/test/java/com/admin/catalogo/infrastructure/ApplicationTest.java
@@ -3,14 +3,14 @@ package com.admin.catalogo.infrastructure;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.core.env.AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME;
 
 class ApplicationTest {
 
     @Test
     public void testMain() {
+        System.setProperty(ACTIVE_PROFILES_PROPERTY_NAME, "test");
         Assertions.assertNotNull(new Application());
-
         Application.main(new String[]{});
     }
 
